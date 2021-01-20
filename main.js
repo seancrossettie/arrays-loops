5fcd4858b548ff5fc38a710d43e2af166e59f655
Sean <jb@JBs-MacBook-Pro.local>
2021-01-19 18:54:19 -0600

    added arrays with functions

--- main.js
+++ main.js
@@ -6,4 +6,17 @@ const students = ['Katy', 'Jackie', 'Sean', 'Nathan'];
 // console.log(students[students.length-1]); // Nathan
 
 students[0] = 'Matthew';
-console.log(students);
+// console.log(students);
+
+const valuePrinter = (array, index) => {
+  return (array[index]);
+}
+
+console.log(valuePrinter(students, 2));
+
+
+const isGregHere = (array, element) => {
+  return array.includes(element)
+}
+
+console.log(isGregHere(students, 'Greg'));
