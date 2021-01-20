79f492d9dfc9805990622cea6f4aa058e6f2a9c6
Sean <jb@JBs-MacBook-Pro.local>
2021-01-19 18:46:20 -0600

    added practice arrays

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("Arrays & Loops")
+const students = ['Katy', 'Jackie', 'Sean', 'Nathan'];
+// console.log(students.length);
+
+// ******* ACCESSING ITEMS IN ARRAY
+// console.log(students[1]); // Jackie
+// console.log(students[students.length-1]); // Nathan
+
+students[0] = 'Matthew';
+console.log(students);
