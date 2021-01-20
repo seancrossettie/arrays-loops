9c7c70d5d63043f6c5ba96db6947ec5f4fc5bdeb
Sean <jb@JBs-MacBook-Pro.local>
2021-01-19 20:38:16 -0600

    did some array methods

--- main.js
+++ main.js
@@ -2,4 +2,16 @@ const array1 = ['cow', 'dog', 'cat'];
 const string1 = array1.join(', ');
 const stringToArray1 = string1.split(', ');
 
-console.log(stringToArray1);
+// console.log(stringToArray1);
+
+// .push() & .pop()
+// array1.pop();
+// console.log(array1);
+// array1.push('cat');
+// console.log(array1);
+
+// .unshift() & .shift()
+array1.unshift('worm');
+console.log(array1);
+array1.shift();
+console.log(array1);
